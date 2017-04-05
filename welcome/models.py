112ba0b971228dd180802b91fef6a8026b8017d7
Yanchang <zhaoyc1990@163.com>
2017-04-05 17:59:48 +0800

    小修改

--- welcome/models.py
+++ welcome/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,10 @@ from django.db import models
 class PageView(models.Model):
     hostname = models.CharField(max_length=32)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class GuestBook(models.Model):
+    name = models.CharField('留言者名称', max_length=50)              #留言者名称
+    email = models.CharField('留言者邮箱', max_length=80,default=None)
+    message = models.TextField('留言内容', max_length=500)
+    timemodify = models.DateTimeField(auto_now=True)    #修改时间
+    timestamp = models.DateTimeField(auto_now_add=True) #创建时间
