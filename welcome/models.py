31415482c9d015fcea823dc64c0a22b93f87182c
Yanchang <zhaoyc1990@163.com>
2017-04-10 18:04:28 +0800

    普通提交

--- welcome/models.py
+++ welcome/models.py
@@ -14,14 +14,34 @@ class GuestBook(models.Model):
 	timestamp = models.DateTimeField(auto_now_add=True)	#创建时间	
 
 	def __unicode__(self):
-		return __str__
+		return self.__str__
 	def __str__(self):
-		return name
+		return self.name
 
 class Announcement(models.Model):
 	content = models.CharField('网站公告', max_length=100)
 	timemodify = models.DateTimeField(auto_now=True)
-	
+	timestamp = models.DateTimeField(auto_now_add=True)  # 创建时间
+
 	def __str__(self):
-		return content
-	
+		return self.content
+
+class ArticleCategory(models.Model):
+	name = models.CharField('类型名称', max_length=50)
+	timemodify = models.DateTimeField(auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True)  # 创建时间
+
+class Article(models.Model):
+	name = models.CharField('文章名称', max_length=100)
+	pageviews = models.IntegerField('文章浏览量')
+	isstick   = models.IntegerField('是否顶置',default=0) #0为普通, 1为置顶
+	stickposition = models.IntegerField('置顶位置') #以十的余数,最多十个置顶
+	timemodify = models.DateTimeField(auto_now=True)  # 修改时间
+	timestamp = models.DateTimeField(auto_now_add=True)  # 创建时间
+
+class ArticleRely(models.Model):
+	name = models.CharField('回复者名称', max_length=50)  # 留言者名称
+	email = models.CharField('回复者邮箱', max_length=80, default=None)
+	content = models.TextField('回复内容', max_length=500)
+	timemodify = models.DateTimeField(auto_now=True)  # 修改时间
+	timestamp = models.DateTimeField(auto_now_add=True)  # 创建时间
